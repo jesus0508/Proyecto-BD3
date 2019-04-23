b90a484471f499668d6194299c140e924fadeb0e
Jesus Alarcon <jesus.alarcon3@unmsm.edu.pe>
2019-04-23 12:02:05 -0500

    Auth con MongoDB

---
+++ app/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DBProject;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use Illuminate\Contracts\Auth\Authenticatable;
+use MongoModel;
+
+class User extends MongoModel implements Authenticatable
+{
+    use AuthenticableTrait;
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
